ad2dfcf1ae715212a636d341fe1b833ac9407b79
Lysander Lyu <lvyc@realbom.com>
2025-11-23 22:00:31 +0800

    keymaps: fix the error comments

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -62,8 +62,8 @@ vim.keymap.set("n", "<leader>gb", builtin.git_branches, opts)
 
 -- fugitive
 vim.keymap.set("n", "<leader>gs", ":Git<CR>")           -- git status
-vim.keymap.set("n", "<leader>gS", ":Git stash<CR>")           -- git status
-vim.keymap.set("n", "<leader>gSp", ":Git stash pop<CR>")           -- git status
+vim.keymap.set("n", "<leader>gS", ":Git stash<CR>")           -- git stash 
+vim.keymap.set("n", "<leader>gSp", ":Git stash pop<CR>")           -- git stash pop
 vim.keymap.set("n", "<leader>gcm", ":Git commit<CR>")    -- commit
 vim.keymap.set("n", "<leader>gp", ":Git pull<CR>")      -- push
 local function current_branch()
