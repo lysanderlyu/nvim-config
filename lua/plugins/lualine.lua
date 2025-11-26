bdf4b54103b2f3d81008d6d14a8f1b1614b6e2c2
Lysander Lyu <lvyc@realbom.com>
2025-11-26 16:34:21 +0800

    lualine: add time showing

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -35,7 +35,7 @@ return{
           lualine_a = {'mode'},
           lualine_b = {'branch', 'diff', 'diagnostics'},
           lualine_c = {'filename'},
-          lualine_x = {'encoding', 'fileformat', 'filetype'},
+          lualine_x = {"os.date('%c')", 'data', 'encoding', 'fileformat', 'filetype'},
           lualine_y = {'progress'},
           lualine_z = {'location'}
         },
