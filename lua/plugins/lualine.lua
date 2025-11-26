253cb151a35700fd659d89c978144984d8d946c9
Lysander Lyu <lvyc@realbom.com>
2025-11-26 16:21:46 +0800

    lualine: more settings

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -9,9 +9,43 @@ return{
         options = {
           theme = theme,
         },
+        refresh = {
+              statusline = 1000,
+              tabline = 1000,
+              winbar = 1000,
+              refresh_time = 16, -- ~60fps
+              events = {
+                'WinEnter',
+                'BufEnter',
+                'BufWritePost',
+                'SessionLoadPost',
+                'FileChangedShellPost',
+                'VimResized',
+                'Filetype',
+                'CursorMoved',
+                'CursorMovedI',
+                'ModeChanged',
+              },
+        },
         tabline = {
           lualine_a = { { "tabs", mode = 2 } }, -- show tab index + filename
           lualine_x = {},
+        },
+        sections = {
+          lualine_a = {'mode'},
+          lualine_b = {'branch', 'diff', 'diagnostics'},
+          lualine_c = {'filename'},
+          lualine_x = {'encoding', 'fileformat', 'filetype'},
+          lualine_y = {'progress'},
+          lualine_z = {'location'}
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = {'filename'},
+          lualine_x = {'location'},
+          lualine_y = {},
+          lualine_z = {}
         },
       }
 
