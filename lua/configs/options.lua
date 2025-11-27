2ef8f1c97dd9129457305bad31d2b8a1a1da7938
Lysander Lyu <lvyc@realbom.com>
2025-11-27 20:16:21 +0800

    remote-clipboard: For sharing the clipboard between remote

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -63,3 +63,16 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
+
+-- For sharing the clipboard between remote
+vim.g.clipboard = {
+   name = "OSC 52",
+   copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+   },
+   paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+   },
+}
