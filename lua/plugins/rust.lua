caa6f12735728953ea13e28331518b9e3f1488e7
lysander lyu <lvyc@realbom.com>
2025-12-19 11:55:23 +0800

    rust: add <leader>rc to clean the rust project

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -7,6 +7,7 @@ return{
             vim.keymap.set("n", "<leader>rb", ":!cargo build<CR>", { silent = true })
             vim.keymap.set("n", "<leader>rr", ":!cargo run<CR>", { silent = true })
             vim.keymap.set("n", "<leader>rt", ":!cargo test<CR>", { silent = true })
+            vim.keymap.set("n", "<leader>rc", ":!cargo clean<CR>", { silent = true })
             vim.keymap.set("n", "<leader>rf", ":RustFmt<CR>", { silent = true })
         end
     }
