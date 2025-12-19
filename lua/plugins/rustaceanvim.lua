7d514dcade7b293430ccd1b4cc1bfdaf08abedaa
lysander lyu <lvyc@realbom.com>
2025-12-19 11:52:30 +0800

    rust: using keymap to cargo instead of using the plugin to build rust

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -1,17 +1,14 @@
 return{
   {
     'mrcjkb/rustaceanvim',
+    enabled = false,
     version = '^6', -- Recommended
     lazy = false, -- This plugin is already lazy
 
     keys = {
       -- Run the binary
       {
-        "<leader>rr",
-        function()
-          require("rustaceanvim").lsp.run()
-        end,
-        desc = "Rust: Run",
+        -- "<leader>rr",function() require("rustaceanvim").lsp.run() end, desc = "Rust: Run",
       },
     },
   }
