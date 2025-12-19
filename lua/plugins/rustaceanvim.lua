709e1070246c7ad3a252a3ff1e902d5efd407ba3
lysander lyu <lvyc@realbom.com>
2025-12-19 15:13:17 +0800

    rustaceanvim: add <leader>rd to show diagnositic on rust

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -1,29 +1,27 @@
 return {
   {
     "mrcjkb/rustaceanvim",
-    enabled = true,   -- enable the plugin
     version = "^6",
-    lazy = false,     -- load immediately
-    ft = "rust",  -- filetype trigger
-
+    ft = "rust",
     config = function()
-      -- Buffer-local keymaps
-      local bufnr = vim.api.nvim_get_current_buf()
-      local opts = { noremap = true, silent = true, buffer = bufnr }
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "rust",
+        callback = function(ev)
+          local opts = { buffer = ev.buf, noremap = true, silent = true }
 
-      -- Hover actions
-      vim.keymap.set("n", "K", function()
-        vim.cmd.RustLsp({ "hover", "actions" })
-      end, opts)
+          vim.keymap.set("n", "K", function()
+            vim.cmd.RustLsp({ "hover", "actions" })
+          end, opts)
 
-      -- Close hover buffer
-      vim.keymap.set("n", "q", "<Cmd>close<CR>", opts)
+          vim.keymap.set("n", "<leader>rd", function()
+            vim.cmd.RustLsp("renderDiagnostic")
+          end, opts)
 
-      -- Rust code actions
-      vim.keymap.set("n", "<leader>a", function()
-        vim.cmd.RustLsp("codeAction")
-      end, opts)
+          vim.keymap.set("n", "<leader>a", function()
+            vim.cmd.RustLsp("codeAction")
+          end, opts)
+        end,
+      })
     end,
   }
 }
-
