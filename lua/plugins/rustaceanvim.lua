dc97c50895c6fc10e3200d8d57b8c8393494a672
lysander lyu <lvyc@realbom.com>
2025-12-19 13:54:03 +0800

    rustaceanvim: support rust code manapge hover with K key

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -1,15 +1,29 @@
-return{
+return {
   {
-    'mrcjkb/rustaceanvim',
-    enabled = false,
-    version = '^6', -- Recommended
-    lazy = false, -- This plugin is already lazy
+    "mrcjkb/rustaceanvim",
+    enabled = true,   -- enable the plugin
+    version = "^6",
+    lazy = false,     -- load immediately
+    ft = "rust",  -- filetype trigger
 
-    keys = {
-      -- Run the binary
-      {
-        -- "<leader>rr",function() require("rustaceanvim").lsp.run() end, desc = "Rust: Run",
-      },
-    },
+    config = function()
+      -- Buffer-local keymaps
+      local bufnr = vim.api.nvim_get_current_buf()
+      local opts = { noremap = true, silent = true, buffer = bufnr }
+
+      -- Hover actions
+      vim.keymap.set("n", "K", function()
+        vim.cmd.RustLsp({ "hover", "actions" })
+      end, opts)
+
+      -- Close hover buffer
+      vim.keymap.set("n", "q", "<Cmd>close<CR>", opts)
+
+      -- Rust code actions
+      vim.keymap.set("n", "<leader>a", function()
+        vim.cmd.RustLsp("codeAction")
+      end, opts)
+    end,
   }
 }
+
