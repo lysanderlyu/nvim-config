0394d4b28d942bcebd2432caf9f583c4cdb0742c
lysander lyu <lvyc@realbom.com>
2025-12-25 10:07:33 +0800

    fzf-lua: using enhanced verison of fzf instead of legacy one

---
+++ lua/plugins/fzf-lua.lua
@@ -0,0 +1,80 @@
+return {
+  {
+    "ibhagwan/fzf-lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    ---@module "fzf-lua"
+    ---@type fzf-lua.Config|{}
+    ---@diagnostics disable: missing-fields
+    opts = {
+      keymap = {
+        preview = {
+          ["<C-d>"] = "preview-page-down",
+          ["<C-u>"] = "preview-page-up",
+        },
+      },
+    },
+    ---@diagnostics enable: missing-fields
+    config = function(_, opts)
+      local fzf = require("fzf-lua")
+      fzf.setup(opts)
+
+      -- 🔹 Search clipboard (+ register)
+      vim.keymap.set("n", "<leader>sS", function()
+        local yank = vim.fn.getreg("+")
+        if yank == "" then
+          vim.notify("No yanked text", vim.log.levels.INFO)
+          return
+        end
+        yank = yank:gsub("[\r\n]+$", "") -- trim newline
+        -- optional: escape backslash and double quote
+        yank = yank:gsub([[\]], [[\\]]):gsub([["]], [[\"]])
+
+        fzf.grep({
+          -- search = '"' .. yank .. '"',
+          search = yank,  -- just the raw string
+          prompt = "Search> ",
+          -- rg_opts = "--column --line-number --no-heading --smart-case",
+          fzf_opts = {
+              ["--ansi"] = "",
+              ["--layout"] = "reverse",
+              ["--info"] = "default",
+          },
+            
+          winopts = {
+            layout = "horizontal",
+            preview = { layout = "vertical", vertical = "right:50%" },
+          },
+        })
+      end, { desc = "Search clipboard (+ register) with native preview" })
+
+      -- 🔹 Search unnamed register (" register)
+      vim.keymap.set("n", "<leader>ss", function()
+        local yank = vim.fn.getreg('"')
+        if yank == "" then
+          vim.notify("No yanked text", vim.log.levels.INFO)
+          return
+        end
+        yank = yank:gsub("[\r\n]+$", "") -- trim newline
+        -- optional: escape backslash and double quote
+        yank = yank:gsub([[\]], [[\\]]):gsub([["]], [[\"]])
+
+        fzf.grep({
+          -- search = '"' .. yank .. '"',
+          search = yank,  -- just the raw string
+          prompt = "Search> ",
+          -- rg_opts = "--column --line-number --no-heading --smart-case",
+          fzf_opts = {
+              ["--ansi"] = "",
+              ["--layout"] = "reverse",
+              ["--info"] = "default",
+          },
+          winopts = {
+            layout = "horizontal",
+            preview = { layout = "vertical", vertical = "right:50%" },
+          },
+        })
+      end, { desc = "Search last yank (\" register) with native preview" })
+    end,
+  },
+}
+
