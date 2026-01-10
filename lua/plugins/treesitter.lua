46f3b6a74f6f2483259bc4538ff64e4d2233a345
Lysander Lyu <lvyc@realbom.com>
2026-01-10 11:24:07 +0800

    treesitter: using master branch for compatible

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,6 +3,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    branch = 'master',
     config = function()
       require("nvim-treesitter.configs").setup {
         ensure_installed = {
