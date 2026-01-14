ad038f695a878966b7282d17dea7e60609f89e63
lysander lyu <lvyc@realbom.com>
2026-01-14 19:40:44 +0800

    rustaceanvim: disable rustaceanvim, using nvim-lspconfig instead

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,7 +2,21 @@ local function on_attach_common(client, bufnr)
     local opts = { noremap=true, silent=true, buffer=bufnr }
 
     -- Go to definition
-    vim.keymap.set('n', '<leader>ls', vim.lsp.buf.definition, opts)
+    -- vim.keymap.set('n', '<leader>ls', vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "<leader>ls", function()
+        local params = vim.lsp.util.make_position_params()
+        vim.lsp.buf_request(0, "textDocument/definition", params, function(err, result, ctx, _)
+            if err or not result or vim.tbl_isempty(result) then
+                print("Definition not found")
+                return
+            end
+    
+            local def = result[1]
+            -- Open in horizontal split
+            vim.cmd("vsplit")  -- horizontal split
+            vim.lsp.util.jump_to_location(def, "utf-8")
+        end)
+    end, opts)
 
     -- Show references
     vim.keymap.set('n', '<leader>lr', vim.lsp.buf.references, opts)
@@ -30,7 +44,7 @@ return {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
-        local servers = { "clangd", "bashls", "pyright", "lua_ls", "jdtls" }
+        local servers = { "clangd", "bashls", "pyright", "lua_ls", "jdtls", "rust_analyzer" }
 
         for _, server in ipairs(servers) do
             vim.lsp.config(server, {
--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -1,30 +1,20 @@
 return {
   {
-    "mrcjkb/rustaceanvim",
-    version = "^6",
-    ft = "rust",
-    config = function()
-      local opts = { noremap = true, silent = true, buffer = true }
-
-      -- Hover
-      vim.keymap.set("n", "K", function()
-        vim.cmd.RustLsp({ "hover", "actions" })
-      end, opts)
-
-      -- Render diagnostics
-      vim.keymap.set("n", "<leader>ld", function()
-        vim.cmd.RustLsp("renderDiagnostic")
-      end, opts)
-
-      -- Rust Doc on HTML
-      vim.keymap.set("n", "<leader>rm", function()
-          vim.cmd.RustLsp('openDocs')
-      end, opts)
-
-      -- Code action
-      vim.keymap.set("n", "<leader>a", function()
-        vim.cmd.RustLsp("codeAction")
-      end, opts)
-    end,
+    -- "mrcjkb/rustaceanvim",
+    -- version = "^6",
+    -- ft = "rust",
+    -- config = function()
+    --   local opts = { noremap = true, silent = true, buffer = true, server = false,}
+    --
+    --   -- Rust Doc on HTML
+    --   vim.keymap.set("n", "<leader>lm", function()
+    --       vim.cmd.RustLsp('openDocs')
+    --   end, opts)
+    --
+    --   -- Code action
+    --   vim.keymap.set("n", "<leader>a", function()
+    --     vim.cmd.RustLsp("codeAction")
+    --   end, opts)
+    -- end,
   }
 }
