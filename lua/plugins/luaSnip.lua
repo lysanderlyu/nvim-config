e149be23a552b9c5673f5136b8b95dd49d15e888
Lysander Lyu <lvyc@realbom.com>
2026-01-22 21:53:38 +0800

    luaSnip: add a dot to indicate the next jumps

--- lua/plugins/luaSnip.lua
+++ lua/plugins/luaSnip.lua
@@ -1,14 +1,49 @@
- return {
+return {
   {
     "L3MON4D3/LuaSnip",
-    version = "v2.*",        -- latest release
+    version = "v2.*",
     build = "make install_jsregexp",
     config = function()
+
       local ls = require("luasnip")
+      local types = require("luasnip.util.types") -- Required for ext_opts
+
+
+      ls.config.set_config({
+        history = true,
+        -- This updates the snippet as you type (great for mirrored text)
+        update_events = "TextChanged,TextChangedI",
+        -- Automatically leave snippet session if you move the cursor outside the region
+        region_check_events = "CursorMoved",
+        delete_check_events = "TextChanged",
+        
+        -- VISUAL FEEDBACK: The "Highlight" part
+
+        ext_opts = {
+          [types.insertNode] = {
+            active = {
+              -- This adds a small virtual dot at the next jump location
+              virt_text = { { "●", "NonText" } },
+            },
+          },
+          [types.choiceNode] = {
+            active = {
+              -- Different icon/color for choice nodes to distinguish them
+              virt_text = { { "󰻵", "WarningMsg" } },
+            },
+
+          },
+
+        },
+      })
+
       require("luasnip.loaders.from_vscode").load({ paths = "~/.config/nvim/snips" })
-      -- Keymaps for expanding/jumping snippets
+
+      -- Your existing Keymaps
+
       vim.keymap.set({"i", "s"}, "<C-k>", function() ls.expand_or_jump() end, {silent = true})
       vim.keymap.set({"i", "s"}, "<C-l>", function() ls.jump(1) end, {silent = true})
+
       vim.keymap.set({"i", "s"}, "<C-j>", function() ls.jump(-1) end, {silent = true})
       vim.keymap.set({"i", "s"}, "<C-e>", function()
         if ls.choice_active() then
