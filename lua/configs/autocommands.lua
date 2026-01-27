b957f71c4a847025b7ffd658b8b180588d1d897a
Lysander Lyu <lvyc@realbom.com>
2026-01-27 16:07:29 +0800

    fugitive: compatible when the path has space on fugitive view

--- lua/configs/autocommands.lua
+++ lua/configs/autocommands.lua
@@ -10,40 +10,68 @@ vim.api.nvim_create_autocmd("BufWritePost", {
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "fugitive",
   callback = function()
+    -- gS = stash file under cursor
+    vim.keymap.set("n", "<leader>gS", function()
+      local file = vim.fn.expand("<cfile>")
+      if file == "" then return end
+      local escaped_file = vim.fn.fnameescape(file)
+      vim.cmd("Git stash -- " .. escaped_file)
+    end, { buffer = true, desc = "Stage file under cursor" })
+
+    -- gSp stash pop up file under cursor
+    vim.keymap.set("n", "<leader>gSp", function()
+      local file = vim.fn.expand("<cfile>")
+      if file == "" then return end
+      local escaped_file = vim.fn.fnameescape(file)
+      vim.cmd("Git stash pop " .. escaped_file)
+    end, { buffer = true, desc = "Stage file under cursor" })
+
     -- ga = stage file under cursor
     vim.keymap.set("n", "<leader>ga", function()
-      vim.cmd("Git add " .. vim.fn.expand("<cfile>"))
+      local file = vim.fn.expand("<cfile>")
+      if file == "" then return end
+      local escaped_file = vim.fn.fnameescape(file)
+      vim.cmd("Git add " .. escaped_file)
     end, { buffer = true, desc = "Stage file under cursor" })
 
     -- gr = reset file from staged under cursor
     vim.keymap.set("n", "<leader>gr", function()
-      vim.cmd("Git reset --mixed -- " .. vim.fn.expand("<cfile>"))
+      local file = vim.fn.expand("<cfile>")
+      if file == "" then return end
+      local escaped_file = vim.fn.fnameescape(file)
+      vim.cmd("Git reset --mixed -- " .. escaped_file)
     end, { buffer = true, desc = "UnStage file under cursor" })
 
     -- gra = reset all the staged files under cursor
     vim.keymap.set("n", "<leader>gra", function()
+      local file = vim.fn.expand("<cfile>")
+      if file == "" then return end
+      local escaped_file = vim.fn.fnameescape(file)
       vim.cmd("Git reset --mixed")
     end, { buffer = true, desc = "UnStage all files" })
 
     -- gco = checkout file under cursor
     vim.keymap.set("n", "<leader>gco", function()
-      vim.cmd("Git checkout -- " .. vim.fn.expand("<cfile>"))
+      local file = vim.fn.expand("<cfile>")
+      if file == "" then return end
+      local escaped_file = vim.fn.fnameescape(file)
+      vim.cmd("Git checkout -- " .. escaped_file)
     end, { buffer = true, desc = "Checkout file under the cursor" })
 
     -- Ctrl-x = open file under cursor in horizontal split
     vim.keymap.set("n", "<C-x>", function()
       local file = vim.fn.expand("<cfile>")
-      if file ~= "" then
-        vim.cmd("split " .. file)
-      end
+      if file == "" then return end
+      local escaped_file = vim.fn.fnameescape(file)
+      vim.cmd("split " .. escaped_file)
     end, { buffer = true, desc = "Open file in split" })
 
     -- Ctrl-v = open file under cursor in vertical split
     vim.keymap.set("n", "<C-v>", function()
       local file = vim.fn.expand("<cfile>")
-      if file ~= "" then
-        vim.cmd("vsplit " .. file)
-      end
+      if file == "" then return end
+      local escaped_file = vim.fn.fnameescape(file)
+      vim.cmd("vsplit " .. escaped_file)
     end, { buffer = true, desc = "Open file in vsplit" })
   end,
 })
