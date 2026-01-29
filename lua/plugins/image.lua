2df8c209d13181d7e89c1c50f968b3fa9f025768
Lysander Lyu <lvyc@realbom.com>
2026-01-29 20:08:00 +0800

    snacks: replace some snacks features with other dedicate plugins

---
+++ lua/plugins/image.lua
@@ -0,0 +1,46 @@
+return {
+  {
+    "3rd/image.nvim",
+    -- IMPORTANT: We disable the build step that requires hererocks/lua 5.1
+    build = false, 
+    config = function()
+      require("image").setup({
+        backend = "kitty", -- Ghostty supports this natively
+        processor = "magick_cli", -- Using CLI bypasses the 'magick' rock error
+        integrations = {
+          markdown = {
+            enabled = true,
+            clear_in_insert_mode = false,
+            download_remote_images = true,
+            only_render_image_at_cursor = false,
+            only_render_image_at_cursor_mode = "popup",
+            floating_windows = false,
+            filetypes = { "markdown", "vimwiki" },
+          },
+          neorg = {
+            enabled = true,
+            filetypes = { "norg" },
+          },
+          typst = {
+            enabled = true,
+            filetypes = { "typst" },
+          },
+          html = { enabled = false },
+          css = { enabled = false },
+        },
+        max_width = nil,
+        max_height = nil,
+        max_width_window_percentage = nil,
+        max_height_window_percentage = 50,
+        scale_factor = 1.0,
+        window_overlap_clear_enabled = false,
+        window_overlap_clear_ft_ignore = { 
+            "cmp_menu", "cmp_docs", "snacks_notif", "scrollview", "scrollview_sign" 
+        },
+        editor_only_render_when_focused = false,
+        tmux_show_only_in_active_window = false,
+        hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" },
+      })
+    end,
+  },
+}
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,34 +1,40 @@
 return {
   {
-    -- "folke/noice.nvim",
-    -- lazy = true,
-    -- event = "VeryLazy",
-    -- dependencies = {
-    --   "MunifTanjim/nui.nvim",   -- required for UI components
-    --   "rcarriga/nvim-notify",   -- optional, for fancy notifications
-    -- },
-    -- config = function()
-    --   require("noice").setup({
-    --   cmdline = {
-    --     format = {
-    --       cmdline = { pattern = "^:", icon = "", lang = "vim" },
-    --       search_down = { kind = "search", pattern = "^/", icon = " " },
-    --       search_up = { kind = "search", pattern = "^%?", icon = " " },
-    --     },
-    --     view = "cmdline_popup",   -- show dynamic command output
-    --     opts = {
-    --       position = { row = 5, col = "50%" },
-    --       size = { width = 60, height = "auto" },
-    --     },
-    --   },
-    --
-    --   messages = {
-    --     view = "notify",  -- or "mini" for small messages
-    --     view_error = "notify",
-    --     view_warn = "notify",
-    --     view_history = "split",  -- optional, history in split
-    --   },
-    -- })
-    -- end,
+    "folke/noice.nvim",
+    lazy = true,
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    config = function()
+      require("noice").setup({
+        cmdline = {
+          format = {
+            cmdline = { pattern = "^:", icon = "", lang = "vim" },
+            search_down = { kind = "search", pattern = "^/", icon = " " },
+            search_up = { kind = "search", pattern = "^%?", icon = " " },
+          },
+          view = "cmdline_popup",
+          opts = {
+            position = { row = 5, col = "50%" },
+            size = { width = 60, height = "auto" },
+          },
+        },
+        messages = {
+          view = "notify",
+          view_error = "notify",
+          view_warn = "notify",
+          view_history = "messages", -- Redirects history to Noice's message view
+        },
+      })
+
+      -- Keymap to show notification history
+      -- Option A: Noice built-in (Opens in a split as per your config)
+      -- vim.keymap.set("n", "<leader>n", "<cmd>Noice history<CR>", { desc = "Show Notification History" })
+
+      -- Option B: If you use Telescope (highly recommended for searching history)
+      vim.keymap.set("n", "<leader>n", "<cmd>Telescope notify<CR>", { desc = "Search Notification History" })
+    end,
   }
 }
