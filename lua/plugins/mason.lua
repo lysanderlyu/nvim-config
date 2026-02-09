3cde7503f7bc9caa162b71fe5c06627fb96c7fd5
Lysander Lyu <lyuyosin@gmail.com>
2026-02-09 16:29:26 +0800

    LSP: using mason to install the LSP server

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -22,18 +22,35 @@ return {
   -- Mason LSP Config
   --------------------------------------------------------
   {
-    -- "williamboman/mason-lspconfig.nvim",
-    -- lazy = false,
-    -- dependencies = { "mason.nvim" },
-    -- opts = {
-    --   ensure_installed = {
-    --     "clangd",       -- C/C++
-    --     "pyright",      -- Python
-    --     "bashls",       -- Bash
-    --     "jdtls",        -- Java
-    --     "lua_ls",       -- Lua
-    --   },
-    --   automatic_installation = true,
-    -- },
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    dependencies = { "mason.nvim" },
+    opts = function()
+      local ensure_installed = {
+        "pyright",      -- Python
+        "bashls",       -- Bash
+        "jdtls",        -- Java
+        "lua_ls",       -- Lua
+      }
+
+      -- Check if we're on Linux ARM64
+      if jit and jit.os == "Linux" and jit.arch == "arm64" then
+        -- Remove clangd from the list for Linux ARM64
+        for i, server in ipairs(ensure_installed) do
+          if server == "clangd" then
+            table.remove(ensure_installed, i)
+            break
+          end
+        end
+      else
+        -- Add clangd for other platforms
+        table.insert(ensure_installed, "clangd")
+      end
+
+      return {
+        ensure_installed = ensure_installed,
+        automatic_installation = true,
+      }
+    end,
   },
 }
