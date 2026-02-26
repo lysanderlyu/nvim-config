3f69f2ba165f1b26f788be530dc1d010bd1632f4
Lysander Lyu <lyuyosin@gmail.com>
2026-02-26 11:46:38 +0800

    plantuml-previewer: add readme

--- lua/plugins/plantuml-previewer.lua
+++ lua/plugins/plantuml-previewer.lua
@@ -1,25 +1,16 @@
 -- plantuml-previewer.lua
 return {
   {
-    'weirongxu/plantuml-previewer.vim',
+    'lysanderlyu/plantuml-previewer.vim',
     dependencies = {
       'nvim-lua/plenary.nvim',
       'aklt/plantuml-syntax',
       'tyru/open-browser.vim',
     },
     config = function()
-      vim.g.plantuml_previewer_open_command = 'echo'
-      vim.g.plantuml_previewer_image_format = 'svg'
-      vim.g.plantuml_previewer_split = 'right'
-      vim.g.plantuml_previewer_auto_refresh = 1
-      vim.g.plantuml_previewer_cache_enabled = 1
-      
-      -- Enable real-time preview
-      vim.g.plantuml_previewer_realtime = 1
-      
       -- Key mappings
-      vim.keymap.set('n', '<leader>ps', ':PlantumlOpen<CR>', { desc = 'PlantUML View' })
-      vim.keymap.set('n', '<leader>pu', ':PlantumlStop<CR>', { desc = 'PlantUML Stop' })
+      vim.keymap.set('n', '<leader>ps', ':PlantumlSave<CR>', { desc = 'PlantUML Save' })
+      vim.keymap.set('n', '<leader>pt', ':PlantumlToggle<CR>', { desc = 'PlantUML Toggle' })
     end
   }
 }
