fba2239fae0742377fb6dcb8d3c04d898df0182e
Lysander Lyu <lyuyosin@gmail.com>
2026-04-23 10:12:57 +0800

    adjust the stored image dir for Pasting Image on codecompanion     This will store the Image under ~/Pictures/agent_assets

--- lua/plugins/img-clip.lua
+++ lua/plugins/img-clip.lua
@@ -19,6 +19,7 @@ return {
           template = "![$LABEL]($FILE_PATH)",
         },
         codecompanion = {
+          dir_path = vim.fn.expand("~/Pictures/agent_assets"),
           prompt_for_file_name = false,
           template = "[Image]($FILE_PATH)",
           use_absolute_path = true,
